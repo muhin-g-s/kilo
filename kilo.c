24cac94e8ad95a7bb467b8a46b22c4238fd0bc1b
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-09-05 01:41:15 +0300

    feat: hide cursor on redraw

--- kilo.c
+++ kilo.c
@@ -114,12 +114,14 @@ void drawRows(struct abuf *ab) {
 void editorRefreshScreen() {
 	struct abuf ab = ABUF_INIT;
 
+	abAppend(&ab, "\x1b[?25l", 6);
   abAppend(&ab, "\x1b[2J", 4);
   abAppend(&ab, "\x1b[H", 3);
 
   drawRows(&ab);
 
   abAppend(&ab, "\x1b[H", 3);
+	abAppend(&ab, "\x1b[?25h", 6);
 
   write(STDOUT_FILENO, ab.b, ab.len);
 
