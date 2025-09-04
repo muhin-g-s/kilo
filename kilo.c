b241400ae621b411437291482b4f04fa0c9ffd2f
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-09-05 02:17:23 +0300

    chore: add editor key

--- kilo.c
+++ kilo.c
@@ -11,6 +11,13 @@
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
+enum editorKey {
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN
+};
+
 struct editorConfig {
 	int cx, cy;
 	int screenrows;
@@ -82,12 +89,19 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
-char editorReadKey() {
+int editorReadKey() {
 	int nread;
 	char c;
 
 	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
     if (nread == -1 && errno != EAGAIN) die("read");
+  }
+
+	switch (c) {
+		case 'a': return ARROW_LEFT;
+    case 'd': return ARROW_RIGHT;
+    case 'w': return ARROW_UP;
+    case 's': return ARROW_DOWN;
   }
 
 	if (c == '\x1b') {
@@ -96,10 +110,10 @@ char editorReadKey() {
     if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
     if (seq[0] == '[') {
       switch (seq[1]) {
-        case 'A': return 'w';
-        case 'B': return 's';
-        case 'C': return 'd';
-        case 'D': return 'a';
+        case 'A': return ARROW_UP;
+        case 'B': return ARROW_DOWN;
+        case 'C': return ARROW_RIGHT;
+        case 'D': return ARROW_LEFT;
       }
     }
     return '\x1b';
@@ -110,33 +124,33 @@ char editorReadKey() {
 
 void editorMoveCursor(int key) {
   switch (key) {
-    case 'a':
+    case ARROW_LEFT:
       E.cx--;
       break;
-    case 'd':
+    case ARROW_RIGHT:
       E.cx++;
       break;
-    case 'w':
+    case ARROW_UP:
       E.cy--;
       break;
-    case 's':
+    case ARROW_DOWN:
       E.cy++;
       break;
   }
 }
 
 void editorProcessKeypress() {
-  char c = editorReadKey();
+  int c = editorReadKey();
   switch (c) {
     case CTRL_KEY('q'):
 			clearScreen();
       exit(0);
       break;
 
-		case 'w':
-    case 's':
-    case 'a':
-    case 'd':
+	case ARROW_UP:
+	case ARROW_DOWN:
+	case ARROW_LEFT:
+	case ARROW_RIGHT:
       editorMoveCursor(c);
       break;
   }
