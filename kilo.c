73355c71f4819a2c49a1d3aa83779baba2ff2024
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-09-06 16:15:44 +0300

    feat: line breaks on enter key pressed

--- kilo.c
+++ kilo.c
@@ -173,10 +173,12 @@ void editorUpdateRow(erow *row) {
   row->rsize = idx;
 }
 
-void editorAppendRow(char *s, size_t len) {
-  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+void editorInsertRow(int at, char *s, size_t len) {
+  if (at < 0 || at > E.numrows) return;
 
-  int at = E.numrows;
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
+
   E.row[at].size = len;
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
@@ -245,12 +247,30 @@ void editorRowDelChar(erow *row, int at) {
 
 void editorInsertChar(int c) {
   if (E.cy == E.numrows) {
-    editorAppendRow("", 0);
+    editorInsertRow(E.numrows, "", 0);
   }
 
   editorRowInsertChar(&E.row[E.cy], E.cx, c);
 
   E.cx++;
+}
+
+void editorInsertNewline() {
+  if (E.cx == 0) {
+    editorInsertRow(E.cy, "", 0);
+  } else {
+    erow *row = &E.row[E.cy];
+
+    editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+
+    row = &E.row[E.cy];
+    row->size = E.cx;
+    row->chars[row->size] = '\0';
+
+    editorUpdateRow(row);
+  }
+  E.cy++;
+  E.cx = 0;
 }
 
 void editorDelChar() {
@@ -291,7 +311,7 @@ void editorOpen(char *filename) {
 			linelen--;
 		}
    
-		editorAppendRow(line, linelen);
+		editorInsertRow(E.numrows, line, linelen);
   }
 
 	E.dirty = 0;
@@ -399,7 +419,7 @@ void editorProcessKeypress() {
 
   switch (c) {
 		case '\r':
-      /* TODO */
+      editorInsertNewline();
       break;
 
     case CTRL_KEY('q'):
@@ -426,7 +446,6 @@ void editorProcessKeypress() {
 		
 		case BACKSPACE:
     case CTRL_KEY('h'):
-      // if (c == DEL_KEY) editorMoveCursor(ARROW_RIGHT);
       editorDelChar();
       break;
 
